5be7c1eff7d42b4f113c9d9d30ab373b5bc0a4a9
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-17 16:08:02 +0300

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,14 +12,14 @@ function onFormSubmit(event) {
     console.log(`${delay.value}, ${step.value}, ${amount.value}`)
   let delayCounter = 0;
   for (let i = 0; i < amount; i += 1){
-    delayCounter = delay + step;
       createPromise(i)
         .then(({ position, delay }) => {
     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
         .catch(({ position, delay }) => {
     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+        });
+    delayCounter = delay + step;
 }
 }
 
